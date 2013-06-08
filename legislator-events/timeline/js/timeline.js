aa4abcc6ab52500ea0cf289367a106a8d5e8f0a7
Peter Darche <pdarche@gmail.com>
2013-06-08 10:56:08 -0400

    Break out Charles Stewart Committee js, refactor

--- legislator-events/timeline/js/timeline.js
+++ legislator-events/timeline/js/timeline.js
@@ -353,55 +353,6 @@ function showEventInfo( eventObj ){
     }
 }
 
-// function eventText( eventObj ){
-// 	var eventType = eventObj.event,
-// 		text;
-	
-// 	switch(eventType){
-// 		case "event/party":
-// 			break
-// 		case "sponsored legislation":
-// 			text = eventObj.info.titles[0][2]
-// 			break
-// 		case "bill cosponsorship":
-// 			text = eventObj.info.official_title
-// 			break
-// 		case "start congressional term":
-// 			text = eventObj.info.party + " " + eventObj.info.type + " from District " + eventObj.info.district + " of " + eventObj.info.state 
-// 			break
-// 		case "joined committee":
-// 			text = "JC"
-// 			break
-// 	}
-
-// 	return text
-// }
-
-// function eventType( eventObj ){
-// 	var eventType = eventObj.event,
-// 		text;
-	
-// 	switch(eventType){
-// 		case "event/party":
-// 			text = " P"
-// 			break
-// 		case "sponsored legislation":
-// 			text = " SL"
-// 			break
-// 		case "bill cosponsorship":
-// 			text = " CSL"
-// 			break
-// 		case "start congressional term":
-// 			text = " BCT"
-// 			break
-// 		case "joined committee":
-// 			text = " JC"
-// 			break
-// 	}
-
-// 	return text
-// }
-
 function compare(a,b) {
   if ( Number(a.time) < Number(b.time))
      return -1;
@@ -588,14 +539,6 @@ function addBills( data ){
 		.style("stroke-width", 1)
 		.style("stroke", "steelblue")
 
-	// event_.append('text')
-	// 	.text("Sponsored Legislation")
-	// 	.attr("transform", "rotate(-135)")
-	// 	.attr("y", 13)
-	// 	.attr("x", 5)
-	// 	.style("fill", "white")
-	// 	.style("font-family", "helvetica")
-
 	event_.on('mouseover', function(d){
 
 		var el = d3.select(this),
@@ -685,14 +628,6 @@ function addCosponsored( data ) {
 		.style("stroke-opacity", 0)
 		.style("stroke-width", 1)
 		.style("stroke", "red")
-
-	// event_.append('text')
-	// 	.text("Cosponsored Legislation")
-	// 	.attr("transform", "rotate(-135)")
-	// 	.attr("y", 13)
-	// 	.attr("x", 5)
-	// 	.style("fill", "white")
-	// 	.style("font-family", "helvetica")
 
 	event_.on('mouseover', function(d){
 
@@ -833,6 +768,7 @@ function addCommittees(){
 
 }
 
+// REFACTOR: NOT NEEDED ANYMORE 
 function addCircles( data ) {
 
 	var event_ = focus.selectAll(".event")
@@ -946,6 +882,7 @@ function addCircles( data ) {
 
 }
 
+// REFACTOR: CHANGE SWITCH TO OBJECT 
 function templateId (d){
 	var data 
 	console.log("incoming data is", d)
@@ -960,6 +897,7 @@ function templateId (d){
 
 			return [ "sponsored_legislation", data ] 
 			break;
+
 		case "event/party":
 			return "red"
 			break;
@@ -1022,6 +960,8 @@ function fixContributorName( name ){
 	return first + ' ' + last
 }
 
+
+// REFACTOR: DEPRECATED
 function addContextCircles( data ) {
 
 	console.log("the data is", data )
@@ -1078,6 +1018,8 @@ function getTimestamp(str) {
   return +new Date(d[0], d[1] - 1, d[2], d[3], d[4], d[5]); // build Date object
 }
 
+
+// REFACTOR: SHOULD BE OBJECT
 function getColor(d){
 
 	switch(d.event) {
@@ -1124,6 +1066,7 @@ var PopupView = Backbone.View.extend({
 
 })
 
+// REFACTOR: CHANGE TO ALLOW FOR ALL EVENT TYPES
 var ExpandedView = Backbone.View.extend({
 	
 	initialize : function(){
@@ -1158,7 +1101,6 @@ var ExpandedView = Backbone.View.extend({
 var options = {
 
 	filter_li : function(){
-		console.log("firing")
 
 		var source = $('#filter_template').html(),
 			template = Handlebars.compile( source )
@@ -1187,8 +1129,11 @@ var addAttributeFilter = {
 	},
 
 	campaign_contribution : function(){
+		var select = '<select class="attribute-drop"' 
+			select += ' id="contribution_attributes_drop"><option>'
+			select += 'Select Attribute</option></select>'
 
-		$('<select class="attribute-drop" id="contribution_attributes_drop"><option>Select Attribute</option></select>').insertAfter('#event_type_filter_drop')
+		$(select).insertAfter('#event_type_filter_drop')
 
 		_.each(contributionAttributes, function(attr){
 			var option = '<option value="' + attr + '"">' + lowerUnderToUpperSpace( attr ) + '</option>';
@@ -1204,8 +1149,11 @@ var addAttributeFilter = {
 	},
 
 	sponsored_legislation : function(){
+		var select = '<select class="attribute-drop"' 
+			select += ' id="contribution_attributes_drop"><option>'
+			select += 'Select Attribute</option></select>'
 
-		$('<select class="attribute-drop" id="sponsored_bills_attributes_drop"><option>Select Attribute</option></select>').insertAfter('#event_type_filter_drop')
+		$(select).insertAfter('#event_type_filter_drop')
 
 		_.each(sponsoredAttributes, function(attr){
 			var option = '<option value="' + attr + '"">' + lowerUnderToUpperSpace( attr ) + '</option>';
@@ -1287,451 +1235,3 @@ var contributionAttributes = [
 
 // to get:
 // all the tracontributor categories
-
-// DETAILED CODE DESCRIPTIONS FOR MEMBER DATA
-
-var cStewartMemberData = {
-	
-	congressNumber : {
-		"103" : "103rd (1993-1995)",
-		"104" : "104th (1995-1997)",
-		"105" : "105th (1997-1999)",
-		"106" : "106th (1999-2001)",
-		"107" : "107th (2001-2003)",
-		"108" : "108th (2003-2005)",
-		"109" : "109th (2005-2007)",
-		"110" : "110th (2007-2009)",
-		"111" : "11th  (2009-2011)"
-	},
-
-	office : {
-		"3" : "Representative",
-		"4" : "Senate, 1st Class",
-		"5" : "Senate, 2nd Class",
-		"6" : "Senate, 3rd Class",
-		"7" : "Delegate",
-		"8" : "Resident Commissioner"
-	},
-	identificationNumber : {
-
-	}, 
-	name : {
-
-	},
-	district : {
-		// "0" : 
-		// "1" :
-		// "2" :
-		// "3" :
-		// "4" : 
-		// "5" : 
-		// "6" : 
-		// "7" : 
-		// "8" : 
-		// "9" : 
-		// "10":
-		// "11":
-		// "12":
-		// "13":
-		// "14":
-		// "15":		
-		// "16":
-		// "17":
-		// "18":
-		// "19":
-		// "20":
-		// "21":
-		// "22":
-		// "23":
-		// "24":
-		// "25":
-		// "26":
-		// "27":
-		// "28":
-		// "29":
-		// "30":
-		// "31":
-		// "32":
-		// "33":
-		"79": "Delegate",
-		"80": "Resident Commissioner",
-		"81": "Senate, 1st Class",
-		"82": "Senate, 2nd Class",
-		"83": "Senate, 3rd Class",
-		"84": "House At-large District"
-	},
-	state : {
-
-	},
-	stateCode : {
-		new_england : {
-			full_name : "New England",
-			states : {
-				"01" : "Connecticut",
-				"02" : "Maine",
-				"03" :	"Massachusetts",
-				"04" :	"New Hampshire",
-				"05" :	"Rhode Island",
-				"06" :	"Vermont"
-			}
-		},
-		middle_atlantic : {
-			full_name: "Middle Atlantic",
-			states : {
-				"11" : "Delaware",
-				"12" : "New Jersey",
-				"13" : "New York",
-				"14" : "Pennsylvania",
-			}
-		},
-		east_north_central : {
-			full_name: "East North Central",
-			states : {
-				"21" : "Illinois",
-				"22" : "Indiana",
-				"23" : "Michigan",
-				"24" : "Ohio",
-				"25" : "Wisconsin",				
-			}
-		},
-		west_north_central : {
-			full_name: "West North Central",
-			states : {
-				"31" : "Iowa",
-				"32" : "Kansas",
-				"33" : "Minnesota",
-				"34" : "Missouri",
-				"35" : "Nebraska",
-				"36" : "North Dakota",
-				"37" : "South Dakota"			
-			}
-		},
-		solid_south : {
-			full_name : "Solid South",
-			states : {
-				"41" : "Alabama",
-				"42" : "Arkansas",
-				"43" : "Florida",
-				"44" : "Georgia",
-				"45" : "Louisiana",
-				"46" : "Mississippi",
-				"47" : "North Carolina",
-				"48" : "South Carolina",
-				"49" : "Texas",
-				"40" : "Virginia"				
-			}
-		},
-		border_states : {
-			full_name : "Border States",
-			states : {
-				"51" : "Kentucky",
-				"52" : "Maryland",
-				"53" : "Oklahoma",
-				"54" : "Tennessee",
-				"56" : "West Virginia"
-			}
-		},
-		mountain_states : {
-			full_name : "Mountain States",
-			states : {
-				"61" : "Arizona",
-				"62" : "Colorado",
-				"63" : "Idaho",
-				"64" : "Montana",
-				"65" : "Nevada",
-				"66" : "New Mexico",
-				"67" : "Utah",
-				"68" : "Wyoming"
-			}
-		},
-		pacific_states : {
-			full_name : "Pacific States",
-			states : {
-				"71" : "California",
-				"72" : "Oregon",
-				"73" : "Washington",
-				"81" : "Alaska",
-				"82" : "Hawaii"
-			}
-		},
-		territories_districts : {
-			full_name : "Territories/Districts",
-			states : {
-				"55" : "District of Columbia",
-				"91" : "Guam",
-				"92" : "Puerto Rico",
-				"93" : "Virgin Islands",
-				"94" : "American Samoa"
-			}
-		},
-	},
-	
-	partyCode : {
-		"100" : "Democrat",
-		"200" :	"Republican",
-		"328" :	"Independent",
-		"999" : "Unknown or minor third party"
-	},
-	
-	chamberSeniority : "The term served in this Congress for Representatives. The Year served for Senators. This variable represents total (not just continuous) service in the chamber and is calculated from the dates/terms of service given in the Congressional Directory.",
-	
-	periodOfServiceInChamber : {
-		"1"	: "Only period of House service",
-		"2"	: "1st Period of House service",
-		"3"	: "2nd Period of House service",
-		"4"	: "3rd Period of House service",
-		"5"	: "4th Period of House service",
-		"6"	: "Only period of Senate service",
-		"7"	: "1st period of Senate service",
-		"8"	: "2nd period of Senate Service"
-	},
-
-	statusOfMemberInNextCongress : {
-		"1" : "Continued in next congress",
-		"2" : "Defeated for election to next congress",
-		"3" : "Defeated for nomination to next congress",
-		"4" : "Elected to another federal post (Senate, President...)",
-		"5" : "Elected to a state or local post (governor, mayor...)",
-		"6" : "Appointed to another federal post (cabinet...)",
-		"7" : "Appointed to a state or local post",
-		"8" : "Retired from public life",
-		"9" : "Unsuccessful contest for another office",
-		"0" : "Inapplicable. Member died or left chamber before the end of this congress."
-	},	
-
-}
-
-
-
-var cStewartAssignmentData = {
-
-	congressNumber : {
-		"103" : "103rd (1993-1995)",
-		"104" : "104th (1995-1997)",
-		"105" : "105th (1997-1999)",
-		"106" : "106th (1999-2001)",
-		"107" : "107th (2001-2003)",
-		"108" : "108th (2003-2005)",
-		"109" : "109th (2005-2007)",
-		"110" : "110th (2007-2009)",
-		"111" : "11th  (2009-2011)"
-	},
-
-	committeeCodes : {
-		house_of_representatives : {
-			"102" : "Agriculture",
-			"104" : "Appropriations",
-			"106" : [ "Armed Services (103rd, 109-111th)",
-					  "National Security (104th - 108th)" ],
-			"113" :	[ "Banking, Finance and Urban Affairs (103rd)",
-					  "Banking and Financial Services (104th - 106th)",
-					  "Financial Services (106th - 111th)" ],
-			"115" : "Budget", 
-			"120" : "District of Columbia (103rd)",
-			"124" : [ "Education and Labor (103rd, 111th)",
-				      "Economic and Educational Opportunities (104th)", 
-				      "Education and the Workplace (105th - 109th)" ],
-			"128" : [ "Energy and Commerce (103rd, 107th - 111th)",
-					  "Commerce (104th - 106th)" ], 
-			"134" :[ "Foreign Affairs (103rd, 110th-111th)",
-					  "International Relations (104th - 109th)" ],
-			"138" :[ "Government Operations (103rd)",
-					 "Government Reform and Oversight (104th - 109th)",
-					 "Oversight and Government Reform (110th-111th)" ],
-			"142" : [ "House Administration (103rd, 109-111th)",
-					  "House Oversight (104th - 108th)" ], 
-			"156" : "Judiciary",
-			"160" : "Merchant Marine and Fisheries (103rd)",
-			"164" : [ "Natural Resources (103rd, 111th)",
-					"Resources (104h - 109th)" ],
-			"168" : "Post Office and Civil Service (103rd)",
-			"173" : [ "Public Works and Transportation (103rd)",
-					  "Transportation and Infrastructure (104th - 111th)"],
-			"176" : "Rules",
-			"182" : [ "Science, Space, and Technology (103rd)",
-					  "Science (104th - 109th)",
-					  "Science and Technology (110th-111th)" ], 
-			"184" : "Small Business",
-			"186" : "Standards of Official Conduct",
-			"192" : "Veterans Affairs",
-			"196" : "Ways and Means",
-			"242" : "Intelligence (Select)",
-			"251" : "Homeland Security (Select 107th and 108th; Standing, 109th-111th)",
-			"252" : "Energy Independence and Global Warming (Select, 110th-111th)",
-			"253" : "Investigate the Voting Irregularities of August 2, 2007 (Select, 110th)"
-		},
-
-		senate : {
-			"305" : "Agriculture, Nutrition, and Forestry",              
-			"306" : "Appropriations",                                    
-			"308" : "Armed Services",                                    
-			"314" : "Banking, Housing, and Urban Affairs",               
-			"316" : "Budget",                                            
-			"321" : "Commerce, Science, and Transportation",             
-			"330" : "Energy and Natural Resources",                      
-			"332" : "Environment and Public Works",                      
-			"336" : "Finance",                                           
-			"338" : "Foreign Relations",
-			"344" : [ "Governmental Affairs (103rd to 108th)",
-			        "Homeland Security and Governmental Affairs (109th)" ], 
-			"358" : "Judiciary",                                         
-			"362" : [ "Labor and Human Resources (103rd to 106th)",
-					"Health, Education, Labor, and Pensions (107th to 109th)" ],
-			"380" : "Rules and Administration",
-			"381" : [ "Small Business (103rd to 107th)",
-			          "Small Business and Entrepreneurship (108th and 109th)" ],
-			"388" : "Veterans Affairs",                                  
-			"419" : "Aging (Special)",                                   
-			"432" : "Intelligence (Select)",                             
-			"434" : "Ethics (Select)",
-			"435" : "Indian Affairs (Select)"
-		},
-
-		joint_committees : {
-			"500" : "Library",
-			"501" : "Printing",
-			"503" : "Taxation",
-			"507" : "Economic"
-		},
-
-		leadership : {
-			"661" : "House Party Leadership",              
-			"662" : "Senate Party Leadership"
-		},
-
-		no_assignment : {
-			"770" : "Elected, not sworn",
-			"772" : "Resigned before assignment",                        
-			"775" : "Appointed, not sworn",                              
-			"780" : "Sworn, only select committee",                      
-			"790" : "Elected and sworn, not assigned",                   
-			"795" : "Appointed and sworn, not assigned"
-		}
-	},
-
-	identificationNumber : {
-
-	},
-
-	name : {
-
-	},
-
-	partyStatusCode : {
-		original_appointments : {
-			"1" : "Majority",
-			"2" : "Minority",
-			"3" : "Other party"		
-		},
-		members_added_to_committees : {
-			"4" : "Majority addition",
-			"5" : "Minority addition"
-		},
-		members_replacing_departed_members : {
-			"6" : "Majority replacement",
-			"7" : "Minority replacement"
-		}, 
-		other : {
-			"8" : "Other party additions or replacemnts",
-			"0" : "Inapplicable; no committee assignment"
-		}
-	},
-
-	rankWithinPartyStatus : {
-
-	},
-
-	dateOfAssignment : {
-
-	},
-
-	dateOfTermination : {
-
-	},
-
-	seniorPartyMember : {
-		committee_chairman : {
-			"11" : "Only Chairman",
-			"12" : "1st Chairman",
-			"13" : "2nd Chairman",
-			"14" : "3rd Chairman",
-			"16" : "Acting Chairman"
-		},
-		ranking_minority_members : {
-			"21" : "Only ranking minority member",
-			"22" : "1st ranking minority member",
-			"23" : "2nd ranking minority member",
-			"24" : "3rd ranking minority member"
-		},
-
-		speaker_of_the_house : {
-			"31" : "Only Speaker",
-			"32" : "1st Speaker",
-			"33" : "2nd Speaker"
-		},
-
-		majority_leadership : {
-			"41" : "Only Majority Leader",
-			"42" : "1st Majority Leader",
-			"43" : "2nd Majority Leader",
-	        "44" : "3rd Majority Leader",
-	        "51" : "Only Majority Whip",
-			"52" : "1st Majority Whip",
-			"53" : "2nd Majority Whip"
-		},
-
-		minority_leadership : {
-			"61" : "Only Minority Leader",
-			"62" : "1st Minority Leader",
-			"63" : "2nd Minority Leader",
-			"64" : "Only Minority Whip",
-			"65" : "1st Minority Whip",
-			"66" : "2nd Minority Whip"
-		},
-
-		non_standard_committee_senior_party_status : {
-			"81" : "Only Vice Chairman",
-			"82" : "1st Vice Chairman",
-			"83" : "2nd Vice Chairman",
-			"86" : "Co-chairman"
-		}
-	},
-
-	committeeSeniority : "For the House, this is the number of the term presently served by the member on the committee. Each time a member leaves the committee and returns, the number of terms is reset to 1.For the Senate, the unit of measure is the year. It can best be described as the year on the committee as of the assignment date for that entry. For example, a freshman senator assigned at the beginning of a congress to committee would have this variable coded 1. Similarly, a senator have that initial entry coded 1. If he is reappointed to the same committee at the start of the next congress, he is still coded 1 since he is still serving his first year on the committee.",
-
-	committeePeriodOfService : {
-		"1" : "Temporary assignment",
-		"2"	: "Only period of service",
-		"3"	: "First period of service",
-		"4"	: "Second period of service",
-		"5"	: "Third period of service",
-		"0"	: "Inapplicable; no committee assignments"
-	},
-
-	committeeStatusAtEndOfThisCongress : {
-		"1"	: { 
-				house : "Remained on committee until adjournment.",
-				senate : "Remained on committee until next assignments announced."
-			},	
-		"2"	: "Transferred to another committee.",
-		"3"	: "Left committee for no other.",
-		"4"	: "Left committee before departing chamber.",
-		"5"	: "Member died.",
-		"6"	: "Resigned to hold another office.",
-		"7"	: "Resigned to seek another office unsuccessfully.",
-		"8"	: "Resigned, but neither held nor sought another office.",
-		"9"	: "Member lost special election (Senate).",
-		"0"	: "Inapplicable; no committee assignments",
-	},
-
-	committeeContinuityOfAssignmentInNextCongress : {
-		"1" : "Continues on committee (or successor)",
-		"2" : "Continues in congress, but not committee",
-		"3" : "Continues in congress, presently unassigned",
-		"4" : "Committee is eliminated",
-		"0" : "Inapplicable; member died or does not serve in next congress."
-	}
-
-
-}
-
